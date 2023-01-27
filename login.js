57dc8cadf35ee8d8a381efaf56b42ac0e64d47f9
Lydia_Numwali <numwalilydia2022@gmail.com>
2023-01-27 07:39:29 +0200

    initial commit

--- login.js
+++ login.js
@@ -1,13 +1,38 @@
-const http =require('http');
-const hostname='localhost';
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+
+const url="mongodb+srv://Numwali123:Numwali2023@cluster0.5ocowna.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect(){
+    try{
+        await mongoose.connect(url);
+        console.log("Connected to Mongoose");
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+connect();
+const http = require("http");
+const fs = require("fs");
+const qs = require("querystring");
 const port=4000;
-const fs=require('fs');
- const server=http.createServer(function (req,res){
-    fs.readFile('login.html', function(err,data){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(data);
-        return res.end();
-    });
+const hostname='localhost';
+
+const server = http.createServer((req, res) => {
+     fs.readFile("login.html",'utf-8', (err, data) => {
+                if (err){console.log(err)}
+                else{
+                    console.log(data);
+                    res.writeHead(200, { "Content-Type": "text/html" });
+                    res.write(data)
+                    res.end()
+                }
+
+            });
+
 });
 server.listen(port,hostname,()=>{
     console.log(`Server running at http://${hostname}:${port}`)
